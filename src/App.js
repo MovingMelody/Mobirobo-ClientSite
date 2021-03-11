2cef5f380496466a6077ae2e853baf488975833c
Sai Sumanth <saisumanth2303@gmail.com>
2021-03-11 16:26:47 +0530

    switch to new branch - to improve animations & showcase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/navbar/Navbar";
+import MobileBrands from "./components/Brands/mobileBrands";
+import WhyChooseUs from "./components/WhyChooseUs/whyChooseUs";
+import GlobalStyle from "./styles/Global";
+import Footer from "./components/Footer/footer";
+import SwiperSlider from "./components/ImageSlider/swiperSlider";
+import Issues from './components/WhyChooseUs/issuesWeSolve'
+import ProductsCategory from './components/Products/productsCategories'
+class App extends Component {
+  state = {
+    navbarOpen: false,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <GlobalStyle>? fix your screen at affordable price */}
+        <Navbar
+          navbarState={this.state.navbarOpen}
+          handleNavbar={this.handleNavbar}
+        />
+        {/* <DemoCarousel /> */}
+        {/* <ImageSlider /> */}
+        <SwiperSlider />
+        <div className="site-wrapper">
+          <Issues />
+          <div style={{ margin: "30px" }}></div>
+        <ProductsCategory />
+          <WhyChooseUs />
+          <div style={{ margin: "30px" }}></div>
+          <MobileBrands />
+          <div style={{ margin: "30px" }}></div>
+        </div>
+        <Footer />
+        <GlobalStyle />
+      </div>
+    );
+  }
 }
 
 export default App;
